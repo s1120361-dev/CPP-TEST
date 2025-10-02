4274db38c7695fda2ce758e20c3914cab5cc3164
s1120361-dev <s1120361@dmhs.kh.edu.tw>
2025-10-02 14:26:37 +0800

    Add files via upload

---
+++ 1002hw.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 50; i++) {
+        if ((i % 7) == 0)
+            cout << i << " ";
+    }
+    return 0;
+}
